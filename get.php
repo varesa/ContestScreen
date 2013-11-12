8c4dba23740d04c6eb42a5668df949acdab92e75
Esa Varemo <esa@kuivanto.fi>
2013-11-12 17:36:28 +0200

    Doing the templating stuff

--- get.php
+++ get.php
@@ -1,9 +1,23 @@
 <?php
 
 $id = $_GET['id'];
-print($id);;
 
 $template = file_get_contents("screen.html");
+
+$info_url = "info/"  . $id . ".txt";
+$img_url  = "info/"  . $id . ".jpg";
+$task_url = "tasks/" . "task1.txt";
+
+$info = file("info/" . $id . ".txt");
+
+//print_r($info);
+$name = implode(array_slice($info,0,1));
+$text = implode(array_slice($info, 1));
+
+//print("Test: " . $name);
+
+$replacements = array("<cont_name>"=>$name, "<cont_text>"=>$text, "<cont_img>"=>"img_url", "<task>"=>"task text");
+$template = str_replace(array_keys($replacements), $replacements, $template);
 print($template);
 
 ?>
